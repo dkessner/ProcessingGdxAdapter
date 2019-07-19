d45e53ae3523783d19f0a92bbb6274f010a37284
Darren Kessner <Darren.Kessner@marlborough.org>
2019-07-19 10:40:02 -0700

    Test3D blank slate

--- Test3D/core/src/io/github/dkessner/Test3D.java
+++ Test3D/core/src/io/github/dkessner/Test3D.java
@@ -22,20 +22,12 @@ public class Test3D extends PApplet
     @Override
     public void setup()
     {
-        img = loadImage("badlogic.jpg");
-
-        pg = createGraphics(400, 400);
-        drawOffscreen();
     }
 
     @Override
     public void draw()
     {
         background(0);
-
-        // draw the badlogic logo
-
-        image(img, 300, 0, 100, 100);
 
         // draw RGB ellipses
 
@@ -53,61 +45,6 @@ public class Test3D extends PApplet
         fill(240, 100, 100); // blue HSV 
         noStroke();
         ellipse(300, 300, 100, 50);
-
-        // draw the contents of the offscreen buffer
-
-        image(pg, 0, 300, 100, 100);
-
-        drawPolygon();
-        drawRotatedEllipses();
-    }
-
-    public void drawRotatedEllipses()
-    {
-        translate(200, 50); 
-
-        pushMatrix();
-        rotate(PI/4);
-        fill(255, 255, 0); // yellow
-        ellipse(0, 0, 100, 50);
-        popMatrix();
-
-        fill(0, 0, 255);
-        ellipse(0, 0, 50, 25);
-    }
-
-    public void drawPolygon()
-    {
-        colorMode(RGB, 255);
-        beginShape(POLYGON);
-        fill(255, 0, 0);
-        vertex(300, 200);
-        fill(0, 255, 0);
-        vertex(400, 200);
-        fill(0, 0, 255);
-        vertex(350, 250);
-        endShape();
-    }
-
-    public void drawOffscreen()
-    {
-        pg.beginDraw();
-
-        pg.background(50);
-
-        pg.image(img, 300, 0, 100, 100);
-
-        pg.noStroke();
-        pg.fill(255, 0, 0);
-        pg.ellipse(100, 100, 100, 50);
-
-        pg.fill(0, 255, 0);
-        pg.ellipse(200, 200, 100, 50);
-
-        pg.fill(0, 0, 255);
-        pg.ellipse(300, 300, 100, 50);
-
-        pg.endDraw();
     }
 
     @Override
@@ -127,9 +64,6 @@ public class Test3D extends PApplet
     {
         println("processing keyReleased key: " + (int)key + " keyCode: " + keyCode); 
     }
-
-    private PImage img;
-    private PGraphics pg;
 }
 
 
